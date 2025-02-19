fb8ff66eb6e65601a7b13790b28fc928bcb4de60
JustinMora18 <justindaniel1810@gmail.com>
2025-02-19 01:38:42 -0500

    refactor: enhance sidebar navigation and add income/expense input forms

--- app.py
+++ app.py
@@ -1,36 +1,39 @@
 import streamlit as st
+import pandas as pd
+from streamlit_option_menu import option_menu
 
 st.set_page_config(page_title="Personal Finance Dashboard", page_icon="💰", layout="wide")
 
-st.title (" 💰 Personal Fincance Dashboard 💰 ") 
-
-st.sidebar.header('Menu')
-menu = st.sidebar.radio("Chose an option: ", ['Dashboard', 'Transactions', 'Financial Report'])
-
-if menu == 'Dashboard':
-    st.subheader('Dashboard')
-    st.write("Aquí se mostrarán los datos financieros.")
-
-elif menu == 'Transactions':
-    st.subheader('Manage your money!')
-    st.subheader('- What Do You Want to do?')
-
-    with st.expander('Money in'):
-        # transaction_type = st.selectbox("Transaction Type", ["Money In!", "Money Out!"])
-        moneyIn = st.number_input('Enter the income amount ($)', min_value=0.0, format='%.2f')
-        category = st.selectbox('Category', ['Job', 'investment', 'Side job', 'Project', 'Research'])
-        date = st.date_input('Date:')
-        notes = st.text_area('Notes (Optional)', height=70)
+with st.sidebar:
+    selected = option_menu("Main Menu", ['Home', 'Incomes', 'Expenses', 'Investments', 'Financial Report'], menu_icon= 'house', default_index=1)
 
 
-    with st.expander('Money out'):
-        moneyOut = st.number_input('Enter the expend amount ($)', min_value=0.0, format="%.2f")
-        category = st.selectbox('Category', ['Food', 'Rent', 'Hobbies', 'School', 'Others'])
-        date = st.date_input('Date')
-        notes = st.text_area('Notes (Optional)', height=70)
 
+if selected == 'Home':
+    st.title (" 💰 Personal Fincance Dashboard 💰 ") 
+    #grapg and features here 
+
+
+elif selected == 'Incomes':
+    st.subheader('Incomes 💵')
+    amount = st.number_input('Enter the income amount ($)', min_value=0.0, format='%.2f')
+    date = st.date_input('Date:')
+    incName = st.text_input('Enter Income Name:')
+    category = st.selectbox('Category', ['Job', 'teaching', 'Side job Two', 'Project', 'Research', 'Finantial Aid'])
+    note  = st.text_area('Notes (Optional)', height=70)
     st.success('Your Transaction has been added successfully!')
 
-elif menu == 'Financial Report':
+
+elif selected == 'Expenses':
+    st.subheader('  Expenses 💵')
+    moneyOut = st.number_input('Enter the expense amount ($)', min_value=0.0, format="%.2f")
+    location = st.text_input('Enter Location:')
+    date = st.date_input('Date')
+    category = st.selectbox('Category', ['Food', 'Rent', 'Hobbies', 'School', 'Others'])
+    notes = st.text_area('Notes (Optional)', height=70)
+
+
+elif selected == 'Financial Report':
     st.subheader('Generate Report')
-    st.write('Analysis')
+    #add database, filtering, etc
+
