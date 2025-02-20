fe2760436e3f20304424818aa084b8dd8698ecf5
JustinMora18 <justindaniel1810@gmail.com>
2025-02-19 22:13:57 -0500

    enhance income and expense input forms with category buttons and improved layout

---
+++ README.md
@@ -0,0 +1,70 @@
+** 💸💸 Personal Finance Dashboard 💸💸 ** 
+
+# Instructions to Run the Application
+
+1. **Clone or Download the Project:**
+    -Clone the repository using Git:
+    ```bash
+            git clone <repository_url>
+    ```
+    -Or download the project as a ZIP file and extract it to your desired location.
+
+2. **Navigate to the Project Directory:**
+    -Open your terminal or command prompt and change to the project directory:
+    ```bash
+            cd path/to/your/project
+    ```
+
+3. **Create a Virtual Environment** 
+    -For Windows:
+    ```bash 
+            python -m venv venv
+    ```
+
+    -For MacOS/Linux: 
+    ```bash
+            python3 -m venv venv
+    ```
+
+    -This command creates a virtual environment named venv in your project directory.
+
+4. **Activate The Virtual Environment**
+    -For Windows:
+    ```bash 
+            venv\Scripts\activate
+    ```
+
+    -For MacOS/Linux: 
+    ```bash
+            source venv/bin/activate
+    ```
+    -After activation, your terminal prompt should change to indicate the active environment. 
+
+5. **Install the Required Dependencies**
+    ```bash
+            pip install -r requirements.txt
+    ```
+    -This command installs all the packages listed in the requirements.txt file.
+
+6. **Run the Application**
+    -Streamlit:
+    ```bash
+            streamlit run app.py
+    ```
+
+7. **Deactivate the Virtual Environment (Optional)**
+    -After you're done, deactivate the virtual environment by running: 
+    ```bash
+            deactivate
+    ```
+    -This command returns you to the global Python environment.
+
+
+
+
+**Credits** 
+
+👨🏻‍💻 Justin Mora
+
+
+
--- app.py
+++ app.py
@@ -1,36 +1,72 @@
 import streamlit as st
 import pandas as pd
 from streamlit_option_menu import option_menu
+from PIL import Image
 
-st.set_page_config(page_title="Personal Finance Dashboard", page_icon="💰", layout="wide")
+st.set_page_config(
+    page_title="Personal Finance Dashboard", page_icon="💰", 
+    layout="wide",
+    initial_sidebar_state="expanded")
+
+    #add funtion here
 
 with st.sidebar:
     selected = option_menu("Main Menu", ['Home', 'Incomes', 'Expenses', 'Investments', 'Financial Report'], menu_icon= 'house', default_index=1)
 
-
-
 if selected == 'Home':
     st.title (" 💰 Personal Fincance Dashboard 💰 ") 
-    #grapg and features here 
-
+    
+    #graph and features here 
 
 elif selected == 'Incomes':
-    st.subheader('Incomes 💵')
-    amount = st.number_input('Enter the income amount ($)', min_value=0.0, format='%.2f')
-    date = st.date_input('Date:')
-    incName = st.text_input('Enter Income Name:')
-    category = st.selectbox('Category', ['Job', 'teaching', 'Side job Two', 'Project', 'Research', 'Finantial Aid'])
-    note  = st.text_area('Notes (Optional)', height=70)
-    st.success('Your Transaction has been added successfully!')
+    col = st.columns((1.5, 4.5, 2), gap='medium')
+    with col[0]:
+        st.header("Income Categories")
+        col1, col2 = st.columns (2)
+        with col1:
+            st.button("💸 Salary")
+            st.button("🧑🏾‍🏫 Teaching")
+            st.button("🧑🏾‍💻 Project")
+            st.button("💰 Finantial Aid")
+        with col2:
+            st.button("🎁 Gift")
+            st.button("🧑‍🧑‍🧒 Parents")
+            st.button("🧑🏾‍💻 Online Sales")
+            st.button("🤑 Others")
+            
+    with col[1]:
+        st.subheader('Incomes 💵')
+        incAmount = st.number_input('Enter the income amount ($)', min_value=0.0, format='%.2f')
+        incDate = st.date_input('Date:')
+        incName = st.text_input('Enter Income Name:')
+        incCategory = st.selectbox('Category', ['Job', 'teaching', 'Side job Two', 'Project', 'Research', 'Finantial Aid'])
+        incNote  = st.text_area('Notes (Optional)', height=70)
+        st.success('Your Transaction has been added successfully!')
 
 
 elif selected == 'Expenses':
-    st.subheader('  Expenses 💵')
-    moneyOut = st.number_input('Enter the expense amount ($)', min_value=0.0, format="%.2f")
-    location = st.text_input('Enter Location:')
-    date = st.date_input('Date')
-    category = st.selectbox('Category', ['Food', 'Rent', 'Hobbies', 'School', 'Others'])
-    notes = st.text_area('Notes (Optional)', height=70)
+    col = st.columns((1.5, 4.5, 2), gap='medium')
+    with col[0]:
+        st.header("Expenses Categories")
+        col1, col2 = st.columns (2)
+        with col1:
+            st.button("🏠 Rent")
+            st.button("🍱 Food")
+            st.button("⛽️ Gas")
+            st.button("📚 Education")
+        with col2:
+            st.button("😷 Health")
+            st.button("🎁 Gifts")
+            st.button("💪🏽 Gym")
+            st.button("🍾 Free Time")
+    
+    with col[1]:
+        st.subheader('  Expenses 💵')
+        expAmount = st.number_input('Enter the expense amount ($)', min_value=0.0, format="%.2f")
+        expLocation = st.text_input('Enter Location:')
+        expDate = st.date_input('Date')
+        expCategory = st.selectbox('Category', ['Food', 'Rent', 'Hobbies', 'School', 'Others'])
+        expNotes = st.text_area('Notes (Optional)', height=70)
 
 
 elif selected == 'Financial Report':
